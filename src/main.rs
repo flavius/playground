52cedc431925d951b46fe6c1eaf638c2c2265030
Flavius Aspra <flavius.as@gmail.com>
2019-05-23 22:31:17 +0200

    more generic code around plugins

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,60 @@
-use std::rc::Rc;
+use std::ops::{Deref, DerefMut};
+use std::any::{Any, TypeId};
 
 enum MyPlugin {
     Web(Web),
     Logging(Logging),
 }
 
-trait Plugin {
+trait Plugin : Any + AsAnyPlugin {
     fn run(&mut self);
     fn shutdown(&mut self);
+}
+
+trait AsAnyPlugin {
+    fn as_any(self: &Self) -> &dyn Any;
+    fn as_any_mut(self: &mut Self) -> &mut dyn Any;
+}
+
+impl<T: Plugin + 'static> AsAnyPlugin for T {
+    fn as_any(self: &Self) -> &dyn Any {
+        self
+    }
+    fn as_any_mut(self: &mut Self) -> &mut dyn Any {
+        self
+    }
 }
 
 impl MyPlugin {
     fn run(&mut self, logger: &mut Box<dyn LogWriter>) {
         logger.log_raw("BEFORE casted run".to_owned());
-        self.as_plugin().run();
+        self.as_plugin_mut().run();
         logger.log_raw("AFTER casted run".to_owned());
     }
     fn shutdown(&mut self, logger: &mut Box<dyn LogWriter>) {
         logger.log_raw("BEFORE casted shutdown".to_owned());
-        self.as_plugin().shutdown();
+        self.as_plugin_mut().shutdown();
         logger.log_raw("AFTER casted shutdown".to_owned());
     }
 
-    fn as_logging(&self) -> Option<&Logging> {
-        match *self {
-            MyPlugin::Logging(ref plugin) => Some(&plugin),
-            _ => None,
-        }
-    }
     // match boilerplate is centralized here
     // Avoidable and optimizable with crates such as "enum_dispatch"
-    fn as_plugin(&mut self) -> &mut dyn Plugin {
+    fn as_plugin_mut(&mut self) -> &mut dyn Plugin {
         match self {
             MyPlugin::Web(plugin) => plugin,
             MyPlugin::Logging(plugin) => plugin,
         }
+    }
+    fn as_plugin(&mut self) -> &dyn Plugin {
+        self.as_plugin_mut()
+    }
+    fn plugin_id(&mut self) -> TypeId {
+        let plugin = self.as_plugin_mut();
+        let any_plugin = plugin.as_any();
+        any_plugin.type_id()
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
@@ -75,6 +95,9 @@ impl Logging {
     }
     fn new_context(&self, id: String) -> LoggingContext {
         LoggingContext(id)
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
@@ -117,7 +140,7 @@ impl InMemoryLogger {
     }
 }
 
-struct PluginList(Vec<Rc<MyPlugin>>);
+struct PluginList(Vec<MyPlugin>);
 
 struct LoggingContext(String);
 
@@ -127,28 +150,33 @@ impl PluginList {
             vec![],
         )
     }
-    fn all(&self) -> impl Iterator<Item = &Rc<MyPlugin>> {
-        self.0.iter()
+    fn register(&mut self, mut my_plugin: MyPlugin) {
+        let plugin_id = my_plugin.plugin_id();
+        self.0.push(my_plugin);
     }
-    fn all_mut(&mut self) -> impl Iterator<Item = &mut Rc<MyPlugin>> {
-        self.0.iter_mut()
-    }
-    fn all_rev(&self) -> impl DoubleEndedIterator<Item = &Rc<MyPlugin>> {
-        self.0.iter().rev()
-    }
-    fn all_rev_mut(&mut self) -> impl DoubleEndedIterator<Item = &mut Rc<MyPlugin>> {
-        self.0.iter_mut().rev()
-    }
-    fn register(&mut self, plugin: MyPlugin) {
-        self.0.push(Rc::new(plugin));
-    }
-    fn logging(&self) -> Rc<&Logging> {
-        for plugin in self.all() {
-            if let Some(v) = plugin.as_logging() {
-                return Rc::new(v);
+    fn get_plugin<'a, T: Plugin + 'static + 'a>(&'a mut self) -> Option<&'a mut T> {
+        let type_id = TypeId::of::<T>();
+        for my_plugin in self.0.iter_mut() {
+            if my_plugin.plugin_id() == type_id {
+                let mut plugin = my_plugin.as_plugin_mut();
+                return plugin.as_any_mut().downcast_mut::<T>();
             }
         }
-        unreachable!()
+        None
+    }
+}
+
+impl Deref for PluginList {
+    type Target = Vec<MyPlugin>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for PluginList {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
 
@@ -158,26 +186,31 @@ struct Application {
 }
 
 impl Application {
-    fn new(plugins: PluginList) -> Self {
-        let logging: Rc<&Logging> = plugins.logging();
+    fn new(mut plugins: PluginList) -> Option<Self> {
+        let logging: Option<&mut Logging> = plugins.get_plugin::<Logging>();
+        if logging.is_none() {
+            println!("no logging");
+            return None;
+        }
+        let logging = logging.unwrap();
         let ctx = logging.new_context("application".to_owned());
         let logger = Box::new(logging.new_logger(ctx));
-        Self {
+        Some(Self {
             plugins,
             logger,
-        }
+        })
     }
     fn run(&mut self) {
         self.logger.log_raw("BEFORE run".to_owned());
-        for mut plugin in self.plugins.all_mut() {
-            Rc::<MyPlugin>::get_mut(&mut plugin).unwrap().run(&mut self.logger);
+        for plugin in &mut *self.plugins {
+            plugin.run(&mut self.logger);
         }
         self.logger.log_raw("AFTER run".to_owned());
     }
     fn shutdown(&mut self) {
         self.logger.log_raw("BEFORE shutdown".to_owned());
-        for mut plugin in self.plugins.all_rev_mut() {
-            Rc::<MyPlugin>::get_mut(&mut plugin).unwrap().shutdown(&mut self.logger);
+        for plugin in &mut *self.plugins {
+            plugin.shutdown(&mut self.logger);
         }
         self.logger.log_raw("AFTER shutdown".to_owned());
     }
@@ -188,7 +221,11 @@ fn main() {
     let mut plugins = PluginList::new();
     plugins.register(MyPlugin::Logging(logging));
     plugins.register(MyPlugin::Web(web));
-    let mut app = Application::new(plugins);
+    let app = Application::new(plugins);
+    if app.is_none() {
+        return;
+    }
+    let mut app = app.unwrap();
     app.run();
     app.shutdown();
 }
